32a5fa49aaabec3027b26363bbb704591fe1c9b7
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-06-25 13:03:24 +0200

    añadiendo animaciones personaje al mundo, faltan alimentarse y atacar y arreglar pequeños bugs

--- scripts/Protagonits.gd
+++ scripts/Protagonits.gd
@@ -4,20 +4,36 @@ var speed = 120
 var direction = 0.0
 var jump = 200
 const gravity = 9
+
 @onready var anim = $AnimationPlayer
 @onready var sprite = $Sprite
 
 func _physics_process(delta):
-	direction = Input.get_axis("ui_left","ui_right")
+	direction = Input.get_axis("caminar_izquierda", "caminar_derecha")
 	velocity.x = direction * speed
-	if direction != 0:
+	
+	# Verifica si la animación de protección está en progreso
+	if anim.current_animation == "protected" || anim.current_animation == "sleep" and anim.is_playing():
+		# No cambiar la animación si está en protección
+		pass
+	elif direction != 0:
 		anim.play("walk")
 	else:
 		anim.play("idle")
+
 	sprite.flip_h = direction < 0 if direction != 0 else sprite.flip_h
+
+	if Input.is_action_just_pressed("protegerse"):
+		anim.play("protected")
+	
+	if Input.is_action_just_pressed("mimir"):
+		anim.play("sleep")
 	
 	if is_on_floor() and Input.is_action_pressed("ui_accept"):
 		velocity.y -= jump
-	if !is_on_floor():
+	
+	if not is_on_floor():
 		velocity.y += gravity
+	
 	move_and_slide()
+
