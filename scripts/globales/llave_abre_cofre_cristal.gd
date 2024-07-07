c3dec29815d9e409ab1437b798d0cb592e391508
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-07-07 13:04:19 +0200

    caida y cambio de escena en mas escenas y llave hecha en nivel 1

---
+++ scripts/globales/llave_abre_cofre_cristal.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var señalCoger = $cogeLaLlave
+@onready var anim = $animLlave
+@onready var llave = $Llave
+
+func _ready():
+	# Conectar señales de entrada y salida
+	self.connect("body_entered", Callable(self, "_on_body_entered"))
+	self.connect("body_exited", Callable(self, "_on_body_exited"))
+	señalCoger.visible = false
+	anim.play("llaveMovimiento")
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		señalCoger.visible = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		señalCoger.visible = false
+
+func _process(delta):
+	if señalCoger.visible and Input.is_action_just_pressed("abrirLoQueSea"):
+		señalCoger.visible = false
+		llave.hide()
+		call_deferred("queue_free")
---
+++ scripts/scriptsMundo1-10/finDeEscena.scene2-5.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var next_scene_path = "res://scenes/world1-10/world_1_10.Scene3-5.tscn"  
+
+func _ready():
+	# Conectar la señal body_entered
+	self.body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		change_scene()
+
+func change_scene():
+	# Cambia a la nueva escena
+	get_tree().change_scene_to_file(next_scene_path)
