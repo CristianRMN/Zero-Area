16bafd6a2bde3d7630ea97d7a90f5831139f79c5
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-07-27 14:59:27 +0200

    haciendo escena 4 del nivel 3, falta la logica

---
+++ scripts/scriptsMundo3-10/bloque_movimiento_izquierdaEscena4.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+# Este bloque quita siempre 40 de vida
+
+var move_speed_fast = 180 # Velocidad rápida de movimiento.
+var move_speed_slow = 50 # Velocidad lenta de movimiento.
+var move_distance = 110 # Distancia total de movimiento.
+var start_position = Vector2.ZERO # Posición inicial de la plataforma.
+@onready var anim = $AnimationPlayer
+
+var direction = -1
+var initial_position
+
+func _ready():
+	initial_position = position
+	anim.play("seMueve")
+
+func _process(delta):
+	# Mover la plataforma hacia la izquierda rápidamente y hacia la derecha lentamente
+	if direction == -1:
+		position.x += direction * move_speed_fast * delta
+	else:
+		position.x += direction * move_speed_slow * delta
+	
+	# Cambiar la dirección al alcanzar la distancia máxima
+	if position.x > initial_position.x + move_distance:
+		direction = -1
+	elif position.x < initial_position.x - move_distance:
+		direction = 1
---
+++ scripts/scriptsMundo3-10/rocaMover2Escena4.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var bloque = self
+@onready var areaEmpuje = $zonaEmpuje2
+@onready var señalEmpuje = $zonaEmpuje2/empujaLaRoca2
+var speed = 30
+
+func _ready():
+	señalEmpuje.visible = false
+	areaEmpuje.connect("body_entered", Callable(self, "_on_body_entered"))
+	areaEmpuje.connect("body_exited", Callable(self, "_on_body_exited"))
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		señalEmpuje.visible = true
+		
+func _on_body_exited(body):
+	if body.name == "Player":
+		señalEmpuje.visible = false
+
+func _physics_process(delta):
+	if señalEmpuje.visible and Input.is_action_pressed("abrirLoQueSea"):
+		# Mover el nodo del bloque completo
+		bloque.position.x -= speed * delta
+
+
+	move_and_slide()
